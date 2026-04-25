ec7cbbbdb2beb0a274f0735a217c346d80f88400
DPazC <alexanderpaz996@gmail.com>
2026-04-25 00:13:27 -0500

    refactor: reestructura paquete base a proyecto.nuevaases

--- src/main/java/proyecto/nuevaases/Proyecto1Application.java
+++ src/main/java/proyecto/nuevaases/Proyecto1Application.java
@@ -1,4 +1,4 @@
-package proyecto.proyecto1;
+package proyecto.nuevaases;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/proyecto/nuevaases/controllers/inicio.java
+++ src/main/java/proyecto/nuevaases/controllers/inicio.java
@@ -1,4 +1,4 @@
-package proyecto.proyecto1.controllers;
+package proyecto.nuevaases.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
